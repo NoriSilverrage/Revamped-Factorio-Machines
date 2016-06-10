8538fc5fa5a0999dc5518e21148adacc2b708b86
Nori Silverrage <nori.silverrage@gmail.com>
2016-06-09 20:03:13 -0500

    tweaks for NE

--- RFM-war_0.8.2/prototypes/resistances.lua
+++ RFM-war_0.8.2/prototypes/resistances.lua
@@ -22,6 +22,145 @@ data.raw["electric-turret"]["laser-turret"].resistances =
       }
     }    
 
+data.raw["electric-turret"]["adv-laser-turret"].resistances =
+    {
+      {
+        type = "physical",
+        percent = 35
+      },
+      {
+        type = "acid",
+        percent = 25
+      },
+      {
+        type = "poison",
+        percent = 35
+      },
+      {
+        type = "explosion",
+        percent = 90
+      },
+      {
+        type = "impact",
+        percent = 35
+      }
+    }    
+
+data.raw["electric-turret"]["sniper-laser-turret"].resistances =
+    {
+      {
+        type = "physical",
+        percent = 25
+      },
+      {
+        type = "acid",
+        percent = 25
+      },
+      {
+        type = "poison",
+        percent = 35
+      },
+      {
+        type = "impact",
+        percent = 50
+      }
+    }   
+
+    
+    
+data.raw["ammo-turret"]["adv-ammo-turret"].resistances =
+    {
+      {
+        type = "physical",
+        percent = 30
+      },
+      {
+        type = "acid",
+        percent = 35
+      },
+      {
+        type = "poison",
+        percent = 35
+      },
+      {
+        type = "explosion",
+        percent = 90
+      },
+      {
+        type = "impact",
+        percent = 35
+      }
+    }
+   
+data.raw["ammo-turret"]["sniper-ammo-turret"].resistances =
+    {
+      {
+        type = "physical",
+        percent = 20
+      },
+      {
+        type = "acid",
+        percent = 25
+      },
+      {
+        type = "poison",
+        percent = 25
+      },
+      {
+        type = "impact",
+        percent = 35
+      }
+    }
+   
+data.raw["ammo-turret"]["ac-ammo-turret"].resistances =
+    {
+      {
+        type = "physical",
+        percent = 50
+      },
+      {
+        type = "acid",
+        percent = 25
+      },
+      {
+        type = "poison",
+        percent = 25
+      },
+      {
+        type = "explosion",
+        percent = 90
+      },
+      {
+        type = "impact",
+        percent = 35
+      }
+    }
+
+data.raw["ammo-turret"]["rocket-ammo-turret"].resistances =
+    {
+      {
+        type = "physical",
+        percent = 50
+      },
+      {
+        type = "acid",
+        percent = 25
+      },
+      {
+        type = "poison",
+        percent = 25
+      },
+      {
+        type = "explosion",
+        percent = 90
+      },
+      {
+        type = "impact",
+        percent = 35
+      }
+    }
+    
+  
 data.raw["wall"]["stone-wall"].resistances =
     {
           {
@@ -60,6 +199,44 @@ data.raw["wall"]["stone-wall"].resistances =
     }
 
     
+data.raw["wall"]["reinforced-wall"].resistances =
+    {
+          {
+            type = "physical",
+            decrease = 5,
+            percent = 50
+          },
+          {
+            type = "impact",
+            decrease = 50,
+            percent = 60
+          },
+          {
+            type = "explosion",
+            decrease = 10,
+            percent = 90
+          },
+          {
+            type = "fire",
+            percent = 100
+          },
+          {
+            type = "laser",
+            percent = 100
+          },
+          {
+            type = "poison",
+            decrease = 10,
+            percent = 25
+          },
+          {
+            type = "acid",
+            decrease = 10,
+            percent = 25
+          },
+    }
+
+    
 data.raw["gate"]["gate"].resistances =
     {
           {
@@ -93,6 +270,45 @@ data.raw["gate"]["gate"].resistances =
           {
             type = "acid",
             decrease = 3,
+            percent = 25
+          },
+    }
+
+    
+    
+data.raw["gate"]["reinforced-gate"].resistances =
+    {
+          {
+            type = "physical",
+            decrease = 5,
+            percent = 50
+          },
+          {
+            type = "impact",
+            decrease = 50,
+            percent = 60
+          },
+          {
+            type = "explosion",
+            decrease = 10,
+            percent = 90
+          },
+          {
+            type = "fire",
+            percent = 100
+          },
+          {
+            type = "laser",
+            percent = 100
+          },
+          {
+            type = "poison",
+            decrease = 10,
+            percent = 25
+          },
+          {
+            type = "acid",
+            decrease = 10,
             percent = 25
           },
     }
